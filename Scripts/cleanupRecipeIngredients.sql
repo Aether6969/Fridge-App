04413aa94d837f4ae33547616bf4596e84c9f2d4
mrtn4242 <mrtn@runbox.com>
2024-06-06 18:09:02 +0200

    added some comments

--- Scripts/cleanupRecipeIngredients.sql
+++ Scripts/cleanupRecipeIngredients.sql
@@ -1,3 +1,12 @@
+-- Many recipies contain multiple subrecipes each of which
+-- with their own list of ingredients, e.g. drømmekage consists
+-- of a cake part and a cover part. In this case some ingredients,
+-- e.g. butter and milk in drømmekage, can exist in both parts. 
+-- This script tries to compute the sum of these ingredients
+-- and gather them in one tuple.
+-- However, it fails to correctly identify situations where the
+-- amount is zero (common for pepper), so it is not used.
+
 DROP VIEW IF EXISTS summedMultiples;
 DROP VIEW IF EXISTS multiples;
 
